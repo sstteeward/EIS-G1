8ca2ae7b0056a8e857db71d2575b7c10ae7a70f4
sstteward <sshumiwat.student@asiancollege.edu.ph>
2025-04-22 21:08:07 +0800

    EIS

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php
+    include 'db.php';
+
+    if (isset($_POST['submit'])) {
+        $employeeID = $_POST['id'];
+        $fullName = $_POST['fullname'];
+        $email = $_POST['email'];
+        $position = $_POST['position'];
+        $sex = $_POST['sex'];
+
+        $insert = mysqli_query($conn, "INSERT INTO employeedb (employeeID, fullName, email, position, sex) 
+                                       VALUES ('$employeeID', '$fullName', '$email', '$position', '$sex')");
+
+        if ($insert) {
+            header("Location: employee.html"); 
+            exit();
+        } else {
+            echo "Error inserting data: " . mysqli_error($conn);
+        }
+    } else {
+        echo "Invalid form submission.";
+    }
+?>
